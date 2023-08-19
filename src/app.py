cc36d4cdf729e14e9dfea319178cfadd28cd2cc9
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-08-19 20:13:29 +0000

    API

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorites, Planets, People
 #from models import Person
 
 app = Flask(__name__)
@@ -39,11 +39,84 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
+
+@app.route('/users/<int:users_id>/favorites', methods=['GET'])
+def get_user_favorite():
+    user_favorites = Favorites.query.all()
+    all_favorites = list(map(lambda x: x.serialize(), user_favorites))
+    return jsonify(all_favorites), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    all_peoples = list(map(lambda x: x.serialize(), people))
+    return jsonify(all_peoples), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_single_person(people_id):
+    person = People.query.get(people_id)
+    return jsonify(person.serialize()), 200 
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planets))
+    return jsonify(all_planets), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_single_planet(planets_id):
+    planet = Planets.query.get(planets_id)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/favorite/planet/<int:planets_id>', methods=['POST'])
+def post_favorite_planet(planets_id):
+    planet = Planets.query.get(planets_id)
+    if not planet:
+        raise APIException('Planet not found', status_code=404)
+
+    new_planet = Favorites(name=planet.name, planets_id=planets_id)
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(new_planet.serialize()), 200
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def post_favorite_person(people_id):
+    person = People.query.get(people_id)
+    if not person:
+        raise APIException('Person not found', status_code=404)
+
+    new_person = Favorites(name=person.name, people_id=people_id)
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify(new_person.serialize()), 200
+
+
+@app.route('/favorite/planet/<int:planets_id>', methods=['DELETE'])
+def delete_favorite_planet(planets_id):
+    planet_favorite = Favorites.query.filter_by(planets_id=planets_id).first()
+    if not planet_favorite:
+        raise APIException('Favorite planet not found', status_code=404)
+    
+    db.session.delete(planet_favorite)
+    db.session.commit() 
+    return jsonify(planet_favorite.serialize()), 200
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    people_favorite = Favorites.query.filter_by(people_id=people_id).first()
+    if not people_favorite:
+        raise APIException('Favorite person not found', status_code=404)
+    
+    db.session.delete(people_favorite)
+    db.session.commit()  
+    return jsonify(people_favorite.serialize()), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(120), nullable = True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=True)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +15,67 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name":self.first_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    climate = db.Column(db.String(250))
+    gravity = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    birth_year = db.Column(db.Integer)
+    eye_color = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    hair_color = db.Column(db.String(250))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "mass": self.mass,
         }
